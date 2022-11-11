97164656234f919e9ac38a4c476c0d9b12599cb6
Alejandro Salas <asalas.wd@outlook.com>
2022-11-11 18:54:00 -0500

    Update dimensions and set visibility

--- src/com/java21days/JList.java
+++ src/com/java21days/JList.java
@@ -4,10 +4,15 @@ import javax.swing.*;
 import java.awt.*;
 
 public class JList extends JFrame {
-    Dimension dim = new Dimension(30, 100);
+    Dimension dim = new Dimension(220, 300);
 
     JList(String title, String[] options, String message){
         super(title);
+        setSize(dim);
+        setLocationRelativeTo(null);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
 
     }
 }
